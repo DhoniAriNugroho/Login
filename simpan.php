767e87a5768e3dc0390591aee8cae311e8c905fb
DhoniAriNugroho <dhoniari29@gmail.com>
2017-03-09 23:47:11 +0700

    Update simpan.php

--- simpan.php
+++ simpan.php
@@ -1,39 +1,26 @@
 <?php
-//mulai proses edit data
-
-//cek dahulu, jika tombol simpan di klik
-if(isset($_POST['simpan'])){
- 
- //inlcude atau memasukkan file koneksi ke database
  include('koneksi.php');
- 
- //jika tombol tambah benar di klik maka lanjut prosesnya
 
- $Nomor  = $_POST['Nomor']; //membuat variabel $nim dan datanya dari inputan NIM
- $Nama = $_POST['Nama']; //membuat variabel $nama dan datanya dari inputan Nama Lengkap
- $Alamat  = $_POST['Alamat']; //membuat variabel $kelas dan datanya dari inputan dropdown Kelas
- $Usia = $_POST['Usia']; //membuat variabel $jurusan dan datanya dari inputan dropdown Jurusan
+ $Nomor  = $_POST['Nomor'];
+ $Nama = $_POST['Nama']; 
+ $Alamat  = $_POST['Alamat'];
+ $Usia = $_POST['Usia'];
  
- //melakukan query dengan perintah UPDATE untuk update data ke database dengan kondisi WHERE siswa_id='$id' <- diambil dari inputan hidden id
  $update = mysql_query("UPDATE data SET Nomor='$Nomor', Nama='$Nama', Alamat='$Alamat', Usia='$Usia' WHERE id='$id'") or die(mysql_error());
  
- //jika query update sukses
  if($update){
   
-  echo 'Data berhasil di simpan! ';  //Pesan jika proses simpan sukses
-  echo '<a href="edit.php?id='.$id.'">Kembali</a>'; //membuat Link untuk kembali ke halaman edit
+  echo 'Data berhasil di simpan! ';
+  echo '<a href="update.php?id='.$id.'">Kembali</a>';
   
  }else{
   
-  echo 'Gagal menyimpan data! ';  //Pesan jika proses simpan gagal
-  echo '<a href="edit.php?id='.$id.'">Kembali</a>'; //membuat Link untuk kembali ke halaman edit
+  echo 'Gagal menyimpan data! ';
+  echo '<a href="update.php?id='.$id.'">Kembali</a>';
   
  }
 
-}else{ //jika tidak terdeteksi tombol simpan di klik
-
- //redirect atau dikembalikan ke halaman edit
+}else{
  echo '<script>window.history.back()</script>';
-
 }
 ?>
